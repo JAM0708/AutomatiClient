e4efde56cbdec77e8a409b541fc6cfba39f78ca5
Joseph Morales <moralesjoe12@hotmail.com>
2018-04-21 18:40:53 -0400

    updated endpoint url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ec2-18-188-2-26.us-east-2.compute.amazonaws.com:8080/AutomatiServer/'
+  apiUrl: 'http://18.219.99.26:8060/AutomatiServer/'
 
 };
